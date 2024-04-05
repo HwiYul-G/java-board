1d2fc5aadaaf2aaa6040646bf9ef1d413d4f805e
kim <hwiyul_g@daum.net>
2024-04-05 20:04:29 +0900

    feat(CommentRepository, SpringDataJpaCommentRepository) : Repository Interface와 impl

---
+++ src/main/java/com/y/java_board/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.y.java_board.repository;
+
+import com.y.java_board.domain.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository {
+
+    Comment save(Comment comment);
+    Optional<Comment> findById(Long id);
+    List<Comment> findByArticleId(Long articleId);
+    void deleteById(Long id);
+
+}
