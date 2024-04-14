fbcad1b04663bca0a9cf55adabb9e981f158b8e1
kim <hwiyul_g@daum.net>
2024-04-14 19:24:00 +0900

    feat(UserDetailModel, UserDetailsServiceImpl) : 로그인한 사용자 인증을 위한 UserDetials와 UserDetilasService 구현체

---
+++ src/main/java/com/y/java_board/domain/UserDetailModel.java
@@ -0,0 +1,70 @@
+package com.y.java_board.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailModel implements UserDetails {
+    private final String userEmail;
+    private final String password;
+    private final String nickname;
+    private final String name;
+    // private List<GrantedAuthority> authorities;
+
+    public UserDetailModel(User user) {
+        this.userEmail = user.getEmail();
+        this.password = user.getPassword();
+        this.nickname = user.getNickname();
+        this.name = user.getName();
+        // this.authorities = Stream.of(user.getRoles().split(","))
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // return this.authorities;
+        return null;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/y/java_board/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.y.java_board.service;
+
+import com.y.java_board.domain.User;
+import com.y.java_board.domain.UserDetailModel;
+import com.y.java_board.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        return user.map(UserDetailModel::new).orElseThrow(
+                () -> new UsernameNotFoundException("[사용자 인증 파트] 타당하지 않은 이메일!")
+        );
+    }
+}
