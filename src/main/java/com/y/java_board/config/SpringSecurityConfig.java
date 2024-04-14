34d7f993951b9ec27bf688eaa9328fccd1b21072
kim <hwiyul_g@daum.net>
2024-04-14 19:24:43 +0900

    feat(SpringSecurityConfig) : 로그인 하지 않은 사용자도 접근할 수 있는 자원 등록 + 로그인 시 crediential한지 확인하기 위한 authenticationProvider 빈 등록

--- src/main/java/com/y/java_board/config/SpringSecurityConfig.java
+++ src/main/java/com/y/java_board/config/SpringSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.y.java_board.config;
 
+import com.y.java_board.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +22,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public SpringSecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails admin = User.withUsername("admin")
@@ -34,16 +48,27 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated()
-                )
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+            .httpBasic(HttpBasicConfigurer::disable)
+            .authorizeHttpRequests(auth ->
+                    auth.requestMatchers("/", "/user/register",
+                                    "/webjars/**", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults())
+            .logout(LogoutConfigurer::permitAll);
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
