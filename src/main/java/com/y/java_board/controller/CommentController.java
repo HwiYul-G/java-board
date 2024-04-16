242af89d5e5b90aaec23f477f078803a3f62336d
kim <hwiyul_g@daum.net>
2024-04-16 09:35:36 +0900

    refactor(CommentController, _list.html) : comment update, delete관련 mapping을 적절한 mapping으로 변경.

--- src/main/java/com/y/java_board/controller/CommentController.java
+++ src/main/java/com/y/java_board/controller/CommentController.java
@@ -5,9 +5,7 @@ import com.y.java_board.dto.CommentDto;
 import com.y.java_board.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CommentController {
@@ -25,14 +23,14 @@ public class CommentController {
         return "redirect:/articles/{articleId}";
     }
 
-    @GetMapping("/articles/{articleId}/comments/delete/{id}")
+    @DeleteMapping("/articles/{articleId}/comments/delete/{id}")
     public String delete(@PathVariable Long id, @PathVariable Long articleId){
         commentService.deleteComment(id);
         return "redirect:/articles/{articleId}";
     }
 
 
-    @PostMapping("/articles/{articleId}/comments/update/{id}")
+    @PutMapping("/articles/{articleId}/comments/update/{id}")
     public String update(@PathVariable Long articleId, @PathVariable Long id, String content){
         commentService.updateComment(id, content);
         return "redirect:/articles/{articleId}";
