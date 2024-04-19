1e205aca37777a0ccba9314f11388fa0244bae44
kim <hwiyul_g@daum.net>
2024-04-19 13:42:59 +0900

    feat(UserController) : 사용자 정보 보여주는 페이지 이동 기능 추가, 사용자 정보 업데이트 프로필 폼 이동 기능 추가, 사용자 프로필 업데이트 기능 추가.

--- src/main/java/com/y/java_board/controller/UserController.java
+++ src/main/java/com/y/java_board/controller/UserController.java
@@ -1,43 +1,101 @@
 package com.y.java_board.controller;
 
+import com.y.java_board.config.UserInfoSession;
 import com.y.java_board.domain.User;
 import com.y.java_board.dto.UserDto;
+import com.y.java_board.service.ImageService;
 import com.y.java_board.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Controller
+@RequiredArgsConstructor
+@SessionAttributes("userInfo")
 public class UserController {
 
-    private final UserService service;
+    private final UserService userService;
+    private final ImageService imageService;
     private final PasswordEncoder passwordEncoder;
 
-
-    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
-        this.service = userService;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/user/register")
-    public String showSignUpForm(ModelMap model){
+    public String showSignUpForm(ModelMap model) {
         model.addAttribute("userDto", new User());
         return "user/signup";
     }
 
     @PostMapping("/user/register")
-    public String signUp(UserDto userDto){
+    public String signUp(UserDto userDto) {
         try {
             User user = userDto.toEntity();
             user.setPassword(passwordEncoder.encode(userDto.password()));
 
-            service.register(user);
-        } catch (IllegalStateException e){
+            userService.register(user);
+        } catch (IllegalStateException e) {
             // TODO : 오류 처리
         }
         return "redirect:/";
     }
+
+    @GetMapping("/user/info")
+    public String showUserInfo(ModelMap model, @ModelAttribute("userInfo") UserInfoSession userInfoSession) {
+        String downloadDirectory = "src/main/resources/static/images/profile";
+        try {
+            byte[] profileImageBytes = imageService.getImage(downloadDirectory, userInfoSession.getProfileImage()); // Unhandled exception : java.io.IOException
+            String profileImageBase64 = Base64.getEncoder().encodeToString(profileImageBytes);
+            model.put("profileImage", profileImageBase64);
+        } catch (IOException e) {
+            model.put("profileImage", "");
+        }
+        return "user/info";
+    }
+
+    @GetMapping("/user/profile")
+    public String showUserProfileForm(@ModelAttribute("userInfo") UserInfoSession userInfoSession) {
+        return "user/updateProfile";
+    }
+
+    @PutMapping("/user/profile")
+    public String updateUserProfile(
+            @RequestParam("nickname") String nickname,
+            @RequestParam("profileImg") MultipartFile profileImage,
+            @ModelAttribute("userInfo") UserInfoSession userInfoSession) throws IOException {
+
+        if(profileImage.isEmpty()){
+            userService.updateUserProfile(User.builder()
+                    .nickname(nickname)
+                    .email(userInfoSession.getEmail())
+                    .build());
+        } else{
+            String uploadDirectory = "src/main/resources/static/images/profile";
+            String previousProfileImagePath = userInfoSession.getProfileImage();
+            if(!previousProfileImagePath.isEmpty() && !previousProfileImagePath.equals("default_profile.png")){
+                imageService.deleteImage(uploadDirectory, previousProfileImagePath);
+            }
+            String profileImageString = imageService.saveImageToStorage(uploadDirectory, profileImage);
+
+            userService.updateUserProfile(User.builder()
+                    .profileImage(profileImageString)
+                    .nickname(nickname)
+                    .email(userInfoSession.getEmail())
+                    .build());
+
+            userInfoSession.setProfileImage(profileImageString);
+        }
+
+        userInfoSession.setNickname(nickname);
+        return "redirect:/user/info";
+    }
+
 
 }
