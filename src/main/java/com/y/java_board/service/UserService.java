46ca2c7487a3645c9829fbc3d062036602c39179
kim <hwiyul_g@daum.net>
2024-04-19 13:41:54 +0900

    feat(UserService, CommentRepository) : 사용자 프로필 업데이트 기능 추가. 이때 사용자가 기존에 작성한 댓글, 게시글에 대한 writer를 전부 변경된 writer로 업데이트 해주어야 한다.

--- src/main/java/com/y/java_board/service/UserService.java
+++ src/main/java/com/y/java_board/service/UserService.java
@@ -2,18 +2,23 @@ package com.y.java_board.service;
 
 
 import com.y.java_board.domain.User;
+import com.y.java_board.repository.ArticleRepository;
+import com.y.java_board.repository.CommentRepository;
 import com.y.java_board.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@SessionAttributes("loggedInUser")
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
 
     public User register(User user) throws IllegalStateException {
         userRepository.findByEmail(user.getEmail())
@@ -33,11 +38,41 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("[존재하지 않는 이메일] 해당 이메일의 사용자를 찾을 수 없습니다."));
     }
 
+    public User updateUserProfile(User user){
+        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+        if(userOptional.isPresent()){
+            String existingNickname = userOptional.get().getNickname();
 
-//    public User updateUser(){
-//
-//    }
-//
+            userOptional.get().setNickname(user.getNickname());
+            if(user.getProfileImage() != null) {
+                userOptional.get().setProfileImage(user.getProfileImage());
+            }
+
+            if(!existingNickname.equals(user.getNickname())) {
+                articleRepository.findAll().stream()
+                        .filter(article -> article.getWriter().equals(existingNickname))
+                        .forEach(article -> article.setWriter(user.getNickname()));
+                commentRepository.findAll().stream()
+                        .filter(comment -> comment.getWriter().equals(existingNickname))
+                        .forEach(comment -> comment.setWriter(user.getNickname()));
+            }
+            return userRepository.save(userOptional.get());
+        }
+        throw new IllegalStateException("[존재하지 않는 이메일] 해당 이메일 사용자가 없어서 프로필 정보를 업데이트 할 수 없습니다.");
+    }
+
+    public String getProfileImageURIByEmail(String email) {
+        String profileImageString = "default_profile.png";
+        return userRepository.findByEmail(email).map(User::getProfileImage)
+                .filter(profileImage -> !profileImage.isEmpty())
+                .orElse(profileImageString);
+    }
+
+    public String getNameByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(User::getName).orElseThrow(() -> new IllegalArgumentException("[존재하지 않는 이메일] 해당 이메일의 사용자를 찾을 수 없습니다."));
+    }
+
 //    public void deleteUser(Long id){
 //
 //    }
