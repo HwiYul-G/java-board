8d9a5446469c3b087ec23501b53ff0383aa97a23
kim <hwiyul_g@daum.net>
2024-04-19 13:38:51 +0900

    feat(User, default_profile.png) : User 도메인에 사용자 프로필 정보 추가, 기본 프로필 이미지 사진 추가.

--- src/main/java/com/y/java_board/domain/User.java
+++ src/main/java/com/y/java_board/domain/User.java
@@ -6,6 +6,7 @@ import lombok.*;
 @Entity @Data
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,6 +17,7 @@ public class User {
     private String password;
     private String name;
     private String nickname;
+    private String profileImage;
 
     public User(String email, String password, String name, String nickname){
         this.email = email;
