4360b2ef34bf05979ec2f60b5bd479f3a354b3d7
kim <hwiyul_g@daum.net>
2024-04-20 22:36:29 +0900

    feat(UserController) : user 개인정보 부분에서 내 댓글이 페이징되서 보이는 컨트롤러 + 내 댓글을 바로 삭제할 수 있는 기능 추가

--- src/main/java/com/y/java_board/controller/UserController.java
+++ src/main/java/com/y/java_board/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.y.java_board.controller;
 
 import com.y.java_board.config.UserInfoSession;
+import com.y.java_board.domain.Comment;
 import com.y.java_board.domain.User;
 import com.y.java_board.dto.UserDto;
+import com.y.java_board.service.CommentService;
 import com.y.java_board.service.ImageService;
 import com.y.java_board.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -17,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,6 +30,7 @@ public class UserController {
 
     private final UserService userService;
     private final ImageService imageService;
+    private final CommentService commentService;
     private final PasswordEncoder passwordEncoder;
     private final HttpSession session;
 
@@ -51,11 +56,15 @@ public class UserController {
     }
 
     @GetMapping("/user/info")
-    public String showUserInfo(ModelMap model, @ModelAttribute("userInfo") UserInfoSession userInfoSession) {
+    public String showUserInfo(
+            ModelMap model,
+            @ModelAttribute("userInfo") UserInfoSession userInfoSession,
+            @RequestParam(name = "commentPage", required = false, defaultValue = "1") int commentPage) {
         try {
             byte[] profileImageBytes = imageService.getImage(PROFILE_IMG_DIRECTORY, userInfoSession.getProfileImage()); // Unhandled exception : java.io.IOException
             String profileImageBase64 = Base64.getEncoder().encodeToString(profileImageBytes);
             model.put("profileImage", profileImageBase64);
+            commentsByPage(model, commentPage, userInfoSession.getNickname());
         } catch (IOException e) {
             model.put("profileImage", "");
         }
@@ -73,14 +82,14 @@ public class UserController {
             @RequestParam("profileImg") MultipartFile profileImage,
             @ModelAttribute("userInfo") UserInfoSession userInfoSession) throws IOException {
 
-        if(profileImage.isEmpty()){
+        if (profileImage.isEmpty()) {
             userService.updateUserProfile(User.builder()
                     .nickname(nickname)
                     .email(userInfoSession.getEmail())
                     .build());
-        } else{
+        } else {
             String previousProfileImagePath = userInfoSession.getProfileImage();
-            if(!previousProfileImagePath.isEmpty() && !previousProfileImagePath.equals("default_profile.png")){
+            if (!previousProfileImagePath.isEmpty() && !previousProfileImagePath.equals("default_profile.png")) {
                 imageService.deleteImage(PROFILE_IMG_DIRECTORY, previousProfileImagePath);
             }
             String profileImageString = imageService.saveImageToStorage(PROFILE_IMG_DIRECTORY, profileImage);
@@ -103,9 +112,31 @@ public class UserController {
     public String deleteUser(@PathVariable("email") String email, @ModelAttribute("userInfo") UserInfoSession userInfoSession) throws IOException {
         String profileImg = userInfoSession.getProfileImage();
         userService.deleteUser(email);
-        if(!profileImg.equals("default_profile.png"))
+        if (!profileImg.equals("default_profile.png"))
             imageService.deleteImage(PROFILE_IMG_DIRECTORY, profileImg);
         session.invalidate();
         return "redirect:/";
     }
+
+    @GetMapping("/commentInfo/{commentPageNumber}")
+    public String commentsByPage(ModelMap model, @PathVariable("commentPageNumber") int currentPage, String nickname) {
+        Page<Comment> page = commentService.findCommentByNicknameAndPaginate(nickname, currentPage);
+        long totalItems = page.getTotalElements();
+        int totalPages = page.getTotalPages();
+
+        List<Comment> myComments = page.getContent();
+
+        model.put("myCommentTotalItems", totalItems);
+        model.put("myCommentTotalPages", totalPages);
+        model.put("myCommentCurrentPage", currentPage);
+        model.put("myComments", myComments);
+        return "redirect:/user/info";
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public String deleteCommentById(@PathVariable Long commentId){
+        commentService.deleteComment(commentId);
+        return "redirect:/user/info";
+    }
+
 }
