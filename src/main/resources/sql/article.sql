44273b3a82b1f2645c48cac1254ab18caa154e72
kim <hwiyul_g@daum.net>
2024-04-26 14:28:06 +0900

    local의 mysql에서 h2로 변경

--- src/main/resources/sql/article.sql
+++
@@ -1,9 +0,0 @@
-create table article(
-    id bigint not null,
-    title varchar(255),
-    content varchar(255),
-    writer varchar(255),
-    createdAt datetime(6),
-    updatedAt datetime(6),
-    primary key (id)
-);
