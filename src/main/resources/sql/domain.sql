3b9a35e8278ddc96b0bade8794bc31eceeee9878
kim <hwiyul_g@daum.net>
2024-05-09 11:36:02 +0900

    sql문 관리

---
+++ src/main/resources/sql/domain.sql
@@ -0,0 +1,29 @@
+USE board;
+
+CREATE TABLE article(
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        title VARCHAR(255),
+                        content TEXT,
+                        writer VARCHAR(255),
+                        created_at DATETIME,
+                        updated_at DATETIME
+);
+
+CREATE TABLE comment(
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        writer VARCHAR(255),
+                        content TEXT,
+                        created_at DATETIME,
+                        updated_at DATETIME,
+                        article_id BIGINT,
+                        FOREIGN KEY (article_id) REFERENCES Article(id)
+);
+
+CREATE TABLE user (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      email VARCHAR(45) UNIQUE NOT NULL,
+                      password VARCHAR(255),
+                      name VARCHAR(255),
+                      nickname VARCHAR(255),
+                      profile_image LONGBLOB
+);
--- src/main/resources/sql/user.sql
+++
@@ -1,8 +0,0 @@
-create table user(
-    id bigint not null,
-    email varchar(45) not null primary key,
-    name varchar(255),
-    nickname varchar(255),
-    password varchar(255),
-    profile_image BLOB
-);
